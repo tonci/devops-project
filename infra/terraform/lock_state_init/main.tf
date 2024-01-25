61db5743fdcb9ea64674ba08d9d5d8b5a2cd05b5
Teodor Nikolov <tonci@lab08.com>
2024-01-26 00:00:30 +0100

    Adds terraform for creation of S3 bucket for state and dynamodb for lock.

---
+++ infra/terraform/lock_state_init/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.33"
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "s3_tf_state" {
+  bucket = "tonci-course-tf-state"
+  tags = {
+    purpose = "course"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf_versioned" {
+  bucket = aws_s3_bucket.s3_tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb_tf_state" {
+  name           = "tf_locks"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockId"
+  attribute {
+    name = "LockId"
+    type = "S"
+  }
+  tags = {
+    purpose = "course"
+  }
+}
