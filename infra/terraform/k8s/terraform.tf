841f09363149812d1b267e3c3800400db3953e0f
Teodor Nikolov <tonci@lab08.com>
2024-01-27 22:19:21 +0100

    Adds Terraform for EKS.

---
+++ infra/terraform/k8s/terraform.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.33.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.5"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.3"
+    }
+  }
+
+  backend "s3" {
+    bucket                  = "tonci-course-tf-state"
+    dynamodb_table          = "tf_locks"
+    key                     = "my-terraform-project"
+    region                  = "eu-west-1"
+  }
+
+  required_version = ">= 1.7.0"
+}
+
---
+++ infra/terraform/k8s/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "cluster_version" {
+  description = "AWS EKS cluster version"
+  type        = string
+  default     = "1.28"
+}
